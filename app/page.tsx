262783e201a60307e19b051254e35cc8bcb8fd8e
manishtecholding <manish.pamnani@techholding.co>
2023-07-25 12:15:20 +0530

    feat: Display products using firebase realtime database :sparkles:

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,53 @@
 'use client';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
-import theme from '@/styles/theme';
 import { useEffect, useState } from 'react';
-import {
-    DatabaseReference,
-    DataSnapshot,
-    getDatabase,
-    ref,
-    set,
-    get,
-} from 'firebase/database';
+import { DatabaseReference, DataSnapshot, ref, get } from 'firebase/database';
 import { database } from '@/utils/firebase';
+import styled from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: Arial, sans-serif;
-    background-color: ${(props) => props.theme.colors.background};
-  }
+// Card component
+const Card = styled.div`
+    width: 18.75em;
+    height: 25em;
+    border: 1px solid #ddd;
+    border-radius: 0.5em;
+    padding: 1em;
+    margin: 1em;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+`;
+
+const CardImage = styled.img`
+    width: 100%;
+    height: 12.5em;
+    object-fit: contain;
+    border-radius: 0.5em;
+    margin-bottom: 1em;
+`;
+
+const CardName = styled.h3`
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin-bottom: 0.5em;
+`;
+
+const CardPrice = styled.p`
+    font-size: 1rem;
+    color: #888;
+    margin-bottom: 0.5em;
+`;
+
+const CardDescription = styled.p`
+    font-size: 1rem;
+    color: #555;
+    flex: 1;
+`;
+
+// Card list container
+const CardListContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
 `;
 
 export default function Home() {
@@ -54,10 +83,12 @@ export default function Home() {
         const randomID = crypto.randomUUID();
 
         try {
-            await set(ref(db, 'products/' + randomID), {
-                name: 'T-shirt',
-                price: 0.99,
-            });
+            // await set(ref(db, 'products/' + randomID), {
+            //   name: 'Smartphone',
+            //   price: 699.99,
+            //   description: 'The latest flagship smartphone with a powerful camera and long battery life',
+            //   imageUrl: 'https://m.media-amazon.com/images/I/61L1ItFgFHL.jpg'
+            // });
             console.log('Data added successfully.');
         } catch (error) {
             console.error('Error adding data to Firebase:', error);
@@ -65,15 +96,16 @@ export default function Home() {
     };
 
     return (
-        <div>
+        <CardListContainer>
             {data &&
-                Object.values(data).map((element: any, index) => (
-                    <div key={index} style={{ backgroundColor: 'red' }}>
-                        <p>{element?.name}</p>
-                        <p>{element?.price}</p>
-                    </div>
+                Object.values(data).map((product: any, index) => (
+                    <Card key={index}>
+                        <CardImage src={product.imageUrl} alt={product.name} />
+                        <CardName>{product.name}</CardName>
+                        <CardPrice>{'$ ' + product.price}</CardPrice>
+                        <CardDescription>{product?.description?.length > 125 ? product.description.slice(0,125) + '...' : product?.description}</CardDescription>
+                    </Card>
                 ))}
-            <button onClick={addItemToDatabase}>Add Item to Database</button>
-        </div>
+        </CardListContainer>
     );
 }
---
+++ app/products/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div>Products works!</div>
+  )
+}
+
+export default Products;
